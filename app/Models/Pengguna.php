284535262906dd0ae0d0e5cdd80597c5818b097b
DESKTOP-G66G17F\Jabbar <reza.okovian@gmail.com>
2025-12-20 21:33:08 +0700

    Add api login, register pengguna & CRUD Produk by CSRF Token

--- app/Models/Pengguna.php
+++ app/Models/Pengguna.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Pengguna extends Authenticatable
+
+class Pengguna extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -83,4 +85,24 @@ class Pengguna extends Authenticatable
     {
         return $this->hasMany(Pesanan::class, 'pelanggan_id');
     }
+        /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+    
 }
